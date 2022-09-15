86775d79c7d1946dc24105875519da9967ca52ae
Anthony tahtouh <anthony1.tahtouh@gmail.com>
2022-09-15 19:18:21 +0300

    mobile sidebar link fix

--- src/Components/Sidebar/NavbarMobileView.js
+++ src/Components/Sidebar/NavbarMobileView.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./NavbarMobileView.css";
 import { GiHamburgerMenu } from "react-icons/gi";
+import {Link} from "react-router-dom";
 
 const NavbarMobileView = () => {
   const [open, setOpen] = useState(false);
@@ -21,47 +22,91 @@ const NavbarMobileView = () => {
         <div className="mobile-nav">
           <ul>
             <li className="nav-item">
+              <Link
+              to="/"
+              >
               <span className="sidebar-icon dashboard-icon"></span> Dashboard
+              </Link>
             </li>
 
             <li className="nav-item">
+            <Link
+              to="/posts"
+              >
               <span className="sidebar-icon posts-icon"></span> Posts
+              </Link>
             </li>
 
             <li className="nav-item">
+            <Link
+              to="/galleries"
+              >
               <span className="sidebar-icon galleries-icon"></span> Galleries
+            </Link>
             </li>
 
             <li className="nav-item">
+            <Link
+              to="/instagram"
+              >
               <span className="sidebar-icon instagram-icon"></span> Instagram
+            </Link>
             </li>
 
             <li className="nav-item">
+            <Link
+              to="/facebook"
+              >
               <span className="sidebar-icon facebook-icon"></span> Facebook
+            </Link>
             </li>
 
             <li className="nav-item">
+            <Link
+              to="/twitter"
+              >
               <span className="sidebar-icon twitter-icon"></span> Twitter
+            </Link>
             </li>
 
             <li className="nav-item">
+            <Link
+              to="/create"
+              >
               <span className="sidebar-icon create-icon"></span> Create
+            </Link>
             </li>
 
             <li className="nav-item">
+            <Link
+              to="/products"
+              >
               <span className="sidebar-icon products-icon"></span> Products
+            </Link>
             </li>
 
             <li className="nav-item">
+            <Link
+              to="/settings"
+              >
               <span className="sidebar-icon settings-icon"></span> Settings
+            </Link>
             </li>
 
             <li className="nav-item small-height"> 
+            <Link
+              to="/account"
+              >
               Account
+            </Link>
             </li>
 
-            <li className="nav-item small-height">
-               Support
+            <li className="nav-item small-height"> 
+              <Link
+                to="/support"
+                >
+                Support
+              </Link>
             </li>
 
             <li className="nav-item small-height">
