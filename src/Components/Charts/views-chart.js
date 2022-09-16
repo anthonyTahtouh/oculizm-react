b19a7b2f277f1f7a02cfea9af6e59cfd7b37bd5b
Anthony tahtouh <anthony1.tahtouh@gmail.com>
2022-09-16 19:23:56 +0300

    2 versions of dasboard charts Recharts the top recomended chart library for react and the react google charts same one used on the old wordpress project

---
+++ src/Components/Charts/views-chart.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./views-chart.css";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+  },
+];
+
+const viewsChart = () => {
+
+ return (
+  <div className="chart-section-container">
+ <ResponsiveContainer width="100%" >
+  <LineChart
+    data={data}
+    margin={{
+      top: 5,
+      right: 30,
+      left: 20,
+      bottom: 5,
+    }}
+  >
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="name" />
+    <YAxis />
+    <Tooltip />
+    <Legend />
+    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+  </LineChart>
+</ResponsiveContainer>
+  </div>
+ 
+ );
+};
+
+export default viewsChart;
--- src/Pages/dashboard/dashboard.js
+++ src/Pages/dashboard/dashboard.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./dashboard.css";
+import ViewsChart from "../../Components/Charts/views-chart";
+import GoogleViewsChart from "../../Components/Charts/google-view-charts";
 
 
 const dashboard = () => {
 
   return (
-  <h1>Dashboard</h1>
+    <div>
+      <h1>Recharts Views chart</h1>
+      <ViewsChart/>
+
+      <h1>Google Views chart</h1>
+      <GoogleViewsChart/>
+    </div>
+  
   );
 };
 
