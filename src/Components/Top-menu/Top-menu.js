fd1015326542a0097c5b9b8fa480460c133017c0
Anthony tahtouh <anthony1.tahtouh@gmail.com>
2022-10-04 20:23:31 +0300

    client list dropdown with value change on select

--- src/Components/Top-menu/Top-menu.js
+++ src/Components/Top-menu/Top-menu.js
@@ -1,40 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Top-menu.css";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
 
 
 const AdminBar = () => {
 
-  return (
-    <div className="container-fluid">
-      <div className="admin-bar">
-      <ul>
-            <li>
-                <button style={{color: 'red'}}>System</button>
-            </li>
-            <li>
-                <button>Manage Clients</button>
-            </li>
-            <li>
-                <button>Custom CSS</button>
-            </li>
-            <li>
-                <button>Posts</button>
-            </li>
-            <li>
-                <button>Events</button>
-            </li>
-            <li>
-                <button>Media</button>
-            </li>
+    const clientlist = [
+        {
+            name: "Acumen Collection (83953)",
+            id: "83953",
+        },
+        {
+            name: "Alaabi (77974)",
+            id: "77974",
+        },
+        {
+            name: "Apatchy (57345)",
+            id: "57345",
+        },
+        {
+            name: "Dr Watson CBD (53283)",
+            id: "53283",
+        },
+    ];
 
-            <li>
-                <button>Logs</button>
-            </li>
-        </ul>
-        {/* <button className="oculizm-logo" ></button> */}
-      </div>
-    </div>
-  );
+    const [value, setValue] = useState('Acumen Collection (83953)');
+
+    const handleSelect = (e) => {
+        console.log(e);
+        setValue(e)
+    }
+
+    return (
+        <div className="container-fluid">
+            <div className="admin-bar">
+                <DropdownButton
+                    title={value}
+                    id="dropdown-menu-align-right"
+                    onSelect={handleSelect}
+                >
+                    {clientlist.map(clientlist => (
+                        <Dropdown.Item eventKey={clientlist.name}>{clientlist.name}</Dropdown.Item>
+                    ))}
+
+                </DropdownButton>
+                <ul>
+                    <li>
+                        <button style={{ color: 'red' }}>System</button>
+                    </li>
+                    <li>
+                        <button>Manage Clients</button>
+                    </li>
+                    <li>
+                        <button>Custom CSS</button>
+                    </li>
+                    <li>
+                        <button>Posts</button>
+                    </li>
+                    <li>
+                        <button>Events</button>
+                    </li>
+                    <li>
+                        <button>Media</button>
+                    </li>
+
+                    <li>
+                        <button>Logs</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default AdminBar;
