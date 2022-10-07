5cd9d3cf6241196811c3dd0afe197ac848389cea
Anthony tahtouh <anthony1.tahtouh@gmail.com>
2022-10-07 19:25:37 +0300

    dashboard top products , top posts , top creators and top hashtags with dynamic data coming from a data array

--- src/Pages/dashboard/dashboard.js
+++ src/Pages/dashboard/dashboard.js
@@ -310,6 +310,141 @@ const revenueChartdata = [
   },
 ];
 
+// Top Products data 
+const topProductsdata = [
+  {
+    src: 'https://cdn.shopify.com/s/files/1/0585/2050/2462/products/PineSamplePack.jpg?v=1645522221',
+    caption: 'Pine Furniture, Shelves & Mantels Samples - Sample Pack - All Pine Variants',
+    ordersNumber: 114,
+  },
+  {
+    src: 'https://cdn.shopify.com/s/files/1/0585/2050/2462/products/OakSampleBox.jpg?v=1648543271',
+    caption: 'Oak Shelves & Mantels - Wax Finish Samples - Oak Pack - All Oak Variants',
+    ordersNumber: 84,
+  },
+  {
+    src: 'https://cdn.shopify.com/s/files/1/0585/2050/2462/products/OakSampleBox.jpg?v=1648543271',
+    caption: 'Oak Shelves & Mantels - Wax Finish Samples - Natural Oak',
+    ordersNumber: 75,
+  },
+  {
+    src: 'https://cdn.shopify.com/s/files/1/0585/2050/2462/products/PineSamplePack.jpg?v=1645522221',
+    caption: 'Pine - Rustic and Smooth Wax Finish Samples - Medium Oak',
+    ordersNumber: 60,
+  },
+];
+
+// Top Posts data 
+const topPostsdata = [
+  {
+    src: 'https://app.oculizm.com/wp-content/uploads/2022/07/71950-271299906_442127400908194_4430778143312418858_n-819x1024.jpg',
+    interactionsNumber: 505,
+  },
+  {
+    src: 'https://app.oculizm.com/wp-content/uploads/2022/08/71950-295904681_731277081508151_6155193503685102687_n-819x1024.jpg',
+    interactionsNumber: 457,
+  },
+  {
+    src: 'https://app.oculizm.com/wp-content/uploads/2022/06/71950-277986528_163335436052723_3065899789541569680_n-819x1024.webp',
+    interactionsNumber: 441,
+  },
+  {
+    src: 'https://app.oculizm.com/wp-content/uploads/2022/07/71950-274549977_275194828060748_5754137522733103766_n-819x1024.jpg',
+    interactionsNumber: 440,
+  },
+];
+
+// Top Creators data 
+const topCreatorsdata = [
+  {
+    src: 'https://scontent-fra5-2.xx.fbcdn.net/v/t51.2885-15/280506364_727147425136938_5768818910996507535_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=86c713&_nc_ohc=PJOKFW6NQTQAX-XIvwH&_nc_ht=scontent-fra5-2.xx&edm=AL-3X8kEAAAA&oh=00_AT88qMPzSgPp_ULkFsAbNTlB4TsPt9PfeNbRqhw0fRSfsw&oe=63431B33',
+    username: 'our_wandsworth_home',
+    postsNumber: 7,
+  },
+  {
+    src: 'https://scontent-fra5-2.xx.fbcdn.net/v/t51.2885-15/117724828_175109090804414_6497226056344732760_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=86c713&_nc_ohc=Bp6THzKgTdIAX_IePO7&_nc_ht=scontent-fra5-2.xx&edm=AL-3X8kEAAAA&oh=00_AT_LNa-9whgaDSygbPKi38pWFHTeCESG_7WwueUgcPrAew&oe=6343674B',
+    username: 'thedoctorscottage',
+    postsNumber: 7,
+  },
+  {
+    src: 'https://scontent-fra5-2.xx.fbcdn.net/v/t51.2885-15/271516719_724322918533809_6996943226430624394_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=86c713&_nc_ohc=RmswNU6QY2wAX84KhKL&_nc_ht=scontent-fra5-2.xx&edm=AL-3X8kEAAAA&oh=00_AT-ZzQe3c3hgN4V_Z4UvRXMy1EkwhOXM2AT3OOMiA7yKHA&oe=634440B9',
+    username: 'athomewithharlow',
+    postsNumber: 6,
+  },
+  {
+    src: 'https://scontent-lhr8-1.xx.fbcdn.net/v/t51.2885-15/297235308_148332434486114_7847064112985141909_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=86c713&_nc_ohc=DUfG4Yok1IkAX9ywa2l&_nc_ht=scontent-lhr8-1.xx&edm=AL-3X8kEAAAA&oh=00_AT8MKXTk20FX4VKgT2Tyd_Ai5dmhUdcB4apw5VunnQxLXA&oe=634260ED',
+    username: 'emmaandthegirls_',
+    postsNumber: 5,
+  },
+];
+
+// Top Hashtag data 
+const topHashtagdata = [
+  {
+    hashtag: 'myfcf',
+    hashtagOccNumber: 11,
+  },
+  {
+    hashtag: 'fcf',
+    hashtagOccNumber: 7,
+  },
+  {
+    hashtag: 'Funky Chunky Furniture',
+    hashtagOccNumber: 17,
+  },
+  {
+    hashtag: 'Furniture',
+    hashtagOccNumber: 27,
+  },
+  {
+    hashtag: 'myfcf',
+    hashtagOccNumber: 11,
+  },
+  {
+    hashtag: 'fcf',
+    hashtagOccNumber: 7,
+  },
+  {
+    hashtag: 'Funky Chunky Furniture',
+    hashtagOccNumber: 17,
+  },
+  {
+    hashtag: 'Furniture',
+    hashtagOccNumber: 27,
+  },
+  {
+    hashtag: 'myfcf',
+    hashtagOccNumber: 11,
+  },
+  {
+    hashtag: 'fcf',
+    hashtagOccNumber: 7,
+  },
+  {
+    hashtag: 'Funky Chunky Furniture',
+    hashtagOccNumber: 17,
+  },
+  {
+    hashtag: 'Furniture',
+    hashtagOccNumber: 27,
+  },
+  {
+    hashtag: 'myfcf',
+    hashtagOccNumber: 11,
+  },
+  {
+    hashtag: 'fcf',
+    hashtagOccNumber: 7,
+  },
+  {
+    hashtag: 'Funky Chunky Furniture',
+    hashtagOccNumber: 17,
+  },
+  {
+    hashtag: 'Furniture',
+    hashtagOccNumber: 27,
+  },
+];
 const dashboard = () => {
 
   return (
@@ -468,9 +603,135 @@ const dashboard = () => {
             </div>
             <Chart data={revenueChartdata} chartName="GBP" chartType="barChart" />
           </div>
-
         </div>
       </div>
+
+      <div className="row">
+        <div className="col-xxl-12">
+          <div className="dashboard-content-block">
+            <h2 className="section-title">
+              <span className="top-products-title"></span>
+              Top Products</h2>
+            <div className="section-description">
+              Showing the most shopped products in your shoppable gallery.
+            </div>
+            <div className="row display-flex">
+              {topProductsdata.map(topProductsdata => (
+                <div className="col-xxl-3 col-md-6 col-sm-12 col-with-bottom-pad">
+                  <div className="post">
+                    <a href="/">
+                      <div className="post-image">
+                        <img
+                          src={topProductsdata.src}
+                          className='img-thumbnail'
+                          alt='...'
+                        />
+                        <div className="absolute-text-container">
+                          <h3 className="big-title">{topProductsdata.ordersNumber}</h3>
+                          <h3 className="small-title">Orders</h3>
+                        </div>
+                      </div>
+                      <p className="top-products">
+                        {topProductsdata.caption}
+                      </p>
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-xxl-12">
+          <div className="dashboard-content-block">
+            <h2 className="section-title">
+              <span className="top-posts-title"></span>
+              Top Posts</h2>
+            <div className="section-description">
+              Showing the most popular posts from your shoppable gallery.
+            </div>
+            <div className="row display-flex">
+              {topPostsdata.map(topPostsdata => (
+                <div className="col-xxl-3 col-md-6 col-sm-12 col-with-bottom-pad">
+                  <div className="post">
+                    <a href="/">
+                      <div className="post-image">
+                        <img
+                          src={topPostsdata.src}
+                          className='img-thumbnail'
+                          alt='...'
+                        />
+                        <div className="absolute-text-container">
+                          <h3 className="big-title">{topPostsdata.interactionsNumber}</h3>
+                          <h3 className="small-title">Interactions</h3>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-xxl-12">
+          <div className="dashboard-content-block">
+            <h2 className="section-title">
+              <span className="top-creators-title"></span>
+              Top Content Creators</h2>
+            <div className="section-description">
+              Showing the most influential content creators in your shoppable gallery.
+            </div>
+            <div className="row display-flex">
+              {topCreatorsdata.map(topCreatorsdata => (
+                <div className="col-xxl-3 col-md-6 col-sm-12 col-with-bottom-pad">
+                  <div className="post">
+                    <a href="/">
+                      <div className="post-image">
+                        <img
+                          src={topCreatorsdata.src}
+                          className='rounded-circle'
+                          alt='...'
+                        />
+                        <div className="absolute-text-container">
+                          <h3 className="big-title">{topCreatorsdata.postsNumber}</h3>
+                          <h3 className="small-title">Posts</h3>
+                        </div>
+                      </div>
+                      <p className="creator-username">
+                        @{topCreatorsdata.username}
+                      </p>
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-xxl-12">
+          <div className="dashboard-content-block">
+            <h2 className="section-title">
+              <span className="top-hashtags-title"></span>
+              Top Hashtags</h2>
+            <div className="section-description">
+              Showing the most popular hashtags used in your posts.
+            </div>
+            {topHashtagdata.map(topHashtagdata => (
+              <div className="hashtag">
+                <a target="_blank" href="/"> #{topHashtagdata.hashtag} ({topHashtagdata.hashtagOccNumber})</a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
     </div>
 
   );
