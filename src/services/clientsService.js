de739fb6c9241441b41de468c26b1a4eb7778a84
Anthony tahtouh <anthony1.tahtouh@gmail.com>
2022-11-09 19:37:56 +0200

    adding comments , rename folders , and modal button and dummy data added to the ashboard

---
+++ src/services/clientsService.js
@@ -0,0 +1,33 @@
+export async function getAllClients() {
+
+    try {
+        const response = await fetch('https://app.oculizm.com/wp-admin/admin-ajax.php?action=get_all_clients');
+        return await response.json();
+    } catch (error) {
+        return [];
+    }
+
+}
+
+export async function getAllClientStats() {
+
+    try {
+        const response = await fetch('https://app.oculizm.com/wp-admin/admin-ajax.php?action=get_client_stats');
+        return await response.json();
+    } catch (error) {
+        return [];
+    }
+
+}
+
+export async function getClientTopProducts() {
+
+    try {
+        const response = await fetch('https://app.oculizm.com/wp-admin/admin-ajax.php?action=get_top_products');
+        return await response.json();
+    } catch (error) {
+        return [];
+    }
+
+}
+
